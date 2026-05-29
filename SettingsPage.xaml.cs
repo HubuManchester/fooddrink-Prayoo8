80cc01b4fb45b1932280dc00faf40f44bcceab0a
lxh <1762625246@qq.com>
2026-05-29 15:19:54 +0800

    feat: add SettingsPage with theme switching and large font mode, update AppShell tabs

---
+++ SettingsPage.xaml.cs
@@ -0,0 +1,43 @@
+using maui.Services;
+
+namespace maui;
+
+public partial class SettingsPage : ContentPage
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+        ThemePicker.ItemsSource = new List<string> { "跟随系统", "浅色主题", "深色主题" };
+        ThemePicker.SelectedIndex = 0;
+        LargeFontSwitch.IsToggled = AccessibilityService.LargeTextEnabled;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        AccessibilityService.ApplyFontScale(this);
+    }
+
+    private void OnThemeChanged(object? sender, EventArgs e)
+    {
+        if (Application.Current == null) return;
+        switch (ThemePicker.SelectedIndex)
+        {
+            case 0:
+                Application.Current.UserAppTheme = AppTheme.Unspecified;
+                break;
+            case 1:
+                Application.Current.UserAppTheme = AppTheme.Light;
+                break;
+            case 2:
+                Application.Current.UserAppTheme = AppTheme.Dark;
+                break;
+        }
+    }
+
+    private void OnLargeFontToggled(object? sender, ToggledEventArgs e)
+    {
+        AccessibilityService.LargeTextEnabled = e.Value;
+        AccessibilityService.ApplyFontScale(this);
+    }
+}
